6f512e8da81dca39228936d9e27563e00706e9de
Y <y@gmail.com>
2025-03-25 07:49:42 +0100

    requirements changed

--- text_animations.py
+++ text_animations.py
@@ -169,8 +169,8 @@ ani = FuncAnimation(fig, update, frames=frames, init_func=init, blit=True, inter
 # Export the animation to an MP4 file
 # duration_seconds=4844
 # fps = frames / duration_seconds # Calculate the frames per second
-writer = FFMpegWriter(fps=10, metadata=dict(artist='Me'), bitrate=2500)
-ani.save('./output/animation.mp4', writer=writer)
+#writer = FFMpegWriter(fps=10, metadata=dict(artist='Me'), bitrate=2500)
+#ani.save('./output/animation.mp4', writer=writer)
 
 # Show the animation
 plt.tight_layout()
